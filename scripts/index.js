1199947ceaa0399a26261af2ce4f9baed20ffc16
unknown <cbeta09@list.ru>
2022-05-17 09:50:16 +0500

    создать template-тег

--- scripts/index.js
+++ scripts/index.js
@@ -30,3 +30,43 @@ function formSubmitHandler (evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+const elementTemplate = document.querySelector('#element').content;
+const groupsElements = document.querySelector('.groups__elements');
+const element = elementTemplate.querySelector('.element').cloneNode(true);
+initialCards.forEach(function(card){
+  element.querySelector('.element__mask-group').src = card.link;
+element.querySelector('.element__title').textContent = card.name;
+element.querySelector('.element__like').addEventListener('click', function(evt) {
+    evt.target.classList.toggle('.element__like_active');
+});
+groupsElements.prepend(element);
+})
+
