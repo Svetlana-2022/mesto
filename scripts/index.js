aac119c2fd571845906456538735cf4e09725b1c
unknown <cbeta09@list.ru>
2022-05-21 21:36:43 +0500

    feat: добавить форму создания карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,35 @@
 const popup = document.querySelector('.popup');
+const popupForEdit = document.querySelector('.popup_for_edit');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupCloseIcon = document.querySelector('.popup__close-icon');
+const profileAddButton = document.querySelector('.profile__add-button');
+const popupForCard = document.querySelector('.popup_for_card');
+const popupCloseIconForCard = document.querySelector('.popup__close-icon_for_card');
 
-
-function openPopup() {
+function openPopup(popup) {
     popup.classList.add('popup_opened');
-    nameInput.value = profileInfoTitle.textContent;
-    jobInput.value = profileInfoSubtitle.textContent;
 }
-function closeIconPopup() {
-    popup.classList.remove('popup_opened');
+
+function closeIconPopup(popup) {
+  popup.classList.remove('popup_opened');
 }
-popupCloseIcon.addEventListener('click', closeIconPopup);
-profileEditButton.addEventListener('click', openPopup);
+
+popupCloseIcon.addEventListener('click', () => {
+  closeIconPopup(popupForEdit);
+});
+popupCloseIconForCard.addEventListener('click', () =>{
+  closeIconPopup(popupForCard);
+});
+
+profileEditButton.addEventListener('click', () => {
+  openPopup(popupForEdit);
+  nameInput.value = profileInfoTitle.textContent;
+  jobInput.value = profileInfoSubtitle.textContent;
+});
+
+profileAddButton.addEventListener('click', () => {
+  openPopup(popupForCard);
+});
 
 let formElement = document.querySelector('.form');
 let nameInput = document.querySelector('.form__input_type_name');
@@ -25,7 +42,7 @@ function formSubmitHandler (evt) {
     evt.preventDefault();
     profileInfoTitle.textContent = nameInput.value;
     profileInfoSubtitle.textContent = jobInput.value;
-    closeIconPopup();
+    closeIconPopup(popupForEdit);
 
 }
 
@@ -57,16 +74,39 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ];
-
-const elementTemplate = document.querySelector('#element').content;
 const groupsElements = document.querySelector('.groups__elements');
-const element = elementTemplate.querySelector('.element').cloneNode(true);
 initialCards.forEach(function(card){
-  element.querySelector('.element__mask-group').src = card.link;
-element.querySelector('.element__title').textContent = card.name;
-element.querySelector('.element__like').addEventListener('click', function(evt) {
-    evt.target.classList.toggle('.element__like_active');
-});
+    const elementTemplate = document.querySelector('.element-template').content;
+    const element = elementTemplate.querySelector('.element').cloneNode(true);
+    element.querySelector('.element__mask-group').src = card.link;
+    element.querySelector('.element__title').textContent = card.name;
+    element.querySelector('.element__like').addEventListener('click', function(evt) {
+      evt.target.classList.toggle('element__like_active');
+      console.log(evt.target);
+    });
 groupsElements.prepend(element);
 })
 
+let formElementForCard = document.querySelector('.form_for_card');
+let titleInput = document.querySelector('.form__input_type_title');
+let linkInput = document.querySelector('.form__input_type_link');
+let elementContaner = document.querySelector('.element__container');
+
+// Обработчик «отправки» формы,
+function formForCardSubmitHandler (evt) {
+    evt.preventDefault();
+    const newElementTemplate = document.querySelector('.element-template').content;
+    const newElement = newElementTemplate.querySelector('.element').cloneNode(true);
+    newElement.querySelector('.element__mask-group').src = linkInput.src;
+    newElement.querySelector('.element__title').textContent = titleInput.value;
+    newElement.querySelector('.element__like').addEventListener('click', function(evt) {
+      evt.target.classList.toggle('element__like_active');
+      console.log(evt.target);
+    });
+    groupsElements.prepend(newElement);
+    
+    closeIconPopup(popupForCard);
+
+}
+
+formElementForCard.addEventListener('submit', formForCardSubmitHandler);
