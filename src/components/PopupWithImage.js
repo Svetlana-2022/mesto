3482b525a35990efeb2dc2b9819cf3373a0b36b5
unknown <cbeta09@list.ru>
2022-08-02 19:46:09 +0500

    fix: поправить кнопку like

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -5,16 +5,12 @@ export default class PopupWithImage extends Popup {
         super(popupSelector);
         this._popupImg = this._popupSelector.querySelector('.popup__el-img');
         this._popupCaption = this._popupSelector.querySelector('.popup__el-caption');
-        //this.link = data.link;
-        //this.name = data.name;
     }
     //вставляет в попап картинку
     open({ data }) {
         super.open();
-        //this._popupImg = this._popupSelector.querySelector('.popup__el-img');
         this._popupImg.src = data.link;
         this._popupImg.alt = data.name;
-        //this._popupCaption = this._popupSelector.querySelector('.popup__el-caption');
         this._popupCaption.textContent = data.name;   
     }
     setEventListeners() {
--- src/pages/index.js
+++ src/pages/index.js
@@ -81,3 +81,13 @@ const formSubmitCard = new PopupWithForm({
   }
 });
 formSubmitCard.setEventListeners();
+
+fetch('https://mesto.nomoreparties.co/v1/cohort-46', {
+  headers: {
+    authorization: '30876ae6-9c71-4f9d-a726-3e14bc235ea6'
+  }
+})
+  .then(res => res.json())
+  .then((result) => {
+    console.log(result);
+  });
